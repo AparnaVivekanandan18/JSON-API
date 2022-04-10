1a4f3c3fe3bb76134c0685de6b9ed7e946da4d70
Aparna-Vivekanandan <84588336+Aparna-Vivekanandan@users.noreply.github.com>
2022-04-10 12:23:13 -0300

    Enabling CORS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const application = express();
+const cors = require('cors');
 
 // Setting the local server port number.
 application.listen(9999), function ()
 {
     console.log("Server listening at the port 9999.............")
 }
+// Cross Origin Resourse sharing - Allowing request from another website/domain
+application.use(cors())

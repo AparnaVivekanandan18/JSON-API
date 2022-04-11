e3ee768f052dd07fdabe945d2f3c3c1b66afbfb7
Aparna-Vivekanandan <84588336+Aparna-Vivekanandan@users.noreply.github.com>
2022-04-11 20:55:22 -0300

    updated changes

--- server.js
+++ server.js
@@ -8,15 +8,89 @@ application.use(cors())
 
 // application.use(express.static("public/"));
 
-// creating an endpoint to fetch the JSON contents (
-application.get('/getProductDetails', function(req, res)
+// Endpoint to fetch (GET) the all JSON contents
+// http://localhost:9999/getAllProductDetails
+application.get('/getAllProductDetails', function(req, res)
 {
     fileReadObj.readFile(__dirname + "/JSONFiles/" + "ProductDetails.json", 'utf8', function(err, data)
     {
-        console.log(data);
-        res.send(data); //sending the response from endpoint
+        if(err)
+        {
+            res.send(err)
+        }
+        else
+        {
+            //console.log(data)
+            res.send(data); //sending the response from endpoint
+        }
+
     });
-})
+}
+)
+
+// Endpoint to fetch (GET) specific product details by its name
+// http://localhost:9999/getProductDetailsById/1
+
+application.get('/getProductDetailsById/:productId',function(req,res)
+    {
+        fileReadObj.readFile(__dirname + "/JSONFiles/" + "ProductDetails.json",function(err,data)
+            {
+                if(err)
+                {
+                    console.log(err)
+                }
+                else
+                {
+                    var products = JSON.parse(data);
+                    var fetchedProduct = products["product"+req.params.productId] //forming "product1"
+                    console.log(fetchedProduct)
+                    res.send(fetchedProduct)
+                }
+            }
+        )
+    }
+)
+
+// Endpoint to fetch (GET) available product colours - by giving productname as input
+// http://localhost:9999/getAvailableProductColours/Girls T-shirt
+// http://localhost:9999/getAvailableProductColours/JBL Wireless Earphones
+
+application.get("/getAvailableProductColours/:productName",function(req,res)
+    {
+        fileReadObj.readFile(__dirname + "/JSONFiles/" + "ProductDetails.json",function(err,data)
+        {
+            if(err)
+            {
+
+            }
+            else
+            {
+                var products = JSON.parse(data); // This will return a JAVASCRIPT Array
+                console.log("Endpoint called");
+                var length = Object.keys(products).length;
+                console.log(length)
+
+                for(i=0;i<length;i++)
+                {
+                    var incrementedI = i+1
+                    var appendName = "product"+incrementedI.toString()
+                    console.log(appendName)
+                    if(req.params.productName == products[appendName].productName)
+                    {
+                        res.send("The available colours are "+ products[appendName].productColour)
+                    }
+                }
+            }
+        }
+        )
+    }
+)
+
+// Endpoint to fetch (GET) available product sizes - by giving productname as input
+
+// Endpoint (POST) to insert a new product into JSON
+
+// Endpoint to remove a product (DELETE)- by giving productname as input
 
 
 // Setting the local server port number.
